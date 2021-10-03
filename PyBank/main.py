12b33666039a5ce35a9fb6a3b37b9cbce428c3fa
Shermeen Ajani <shermeen.ajani@gmail.com>
2021-10-02 21:28:47 -0400

    Python Analysis

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,11 +1,11 @@
-#Worked on this section of the assignment with the study group Stuart Smith and Tim Smith
-#create the file path across operating systems
+# Worked on this section of the assignment with the study group Stuart Smith and Tim Smith
+# Create the file path across operating systems
 import os
 
-#the module for reading csv file
+# The module for reading csv file
 import csv
 
-#the path for the csv file
+# The path for the csv file
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
 # Use the Improved Reading using CSV module
@@ -30,54 +30,57 @@ with open(csvpath) as csvfile:
     lastmonthPL=0
     # Initialize the variable for change value to zero
     change=0
-    # Initialize the variable for change value to zero
+    # Initialize the variable for total change of profit/loss value to zero
     totalchangesPL=0
 
     # Read each row of data after the header
     for row in csvreader:
 
-        #if it is the first month, save the Profit/Loss value
+        # If it is the first month, save the Profit/Loss value in column 2
         if count_month ==0:
             PLfirstmonth = int(row[1])
             
-        # Calculate the total number of months in the dataset
+        # Calculate the total number of months in the data set
         count_month+=1
+
         # Calculate the net total amount of Profit/Losses over the entire period
         ProfitLoss+=int(row[1])
 
-        #calculate the Profit/Loss change month over month
+        # Calculate the Profit/Loss change month over month
         change = int(row[1]) - int(lastmonthPL)
 
-        #add the Profit/Loss changes and keep a runnig total of all the Profit/Loss changes
+        # Add the Profit/Loss changes and keep a running total of all the Profit/Loss changes
         totalchangesPL+=change
         
-        #if change is less than the greatest loss value, save it as GreatestLoss and save the month and year
+        # If change is less than the greatest loss value, save it as GreatestLoss and save the month and year
         if change < int(GreatestLoss):
             GreatestLoss = change
             GreatestLossDate = row[0]
 
-        #if change is greater than the greatest profit value, save it as GreatestProfit and save the month and year
+        # If change is greater than the greatest profit value, save it as GreatestProfit and save the month and year
         if change > int(GreatestProfit):
             GreatestProfit = change
             GreatestProfitDate = row[0]
 
-        #save the current Profit/Loss value as last month to calculate the next change
+        # Save the current Profit/Loss value as last month to calculate the next Profit/Loss change
         lastmonthPL = row[1]
-        
-#Calculate the average monthly change
+
+# Calculate the changes in the "Profit/Losses" over the entire period, then find the average of those changes        
+# Calculate the average monthly change
 avg_change = (totalchangesPL-PLfirstmonth)/(count_month-1)
 
 print(f'Financial Analysis')
 print(f'----------------------------')
+# Print the total number months included in the data set
 print(f'Total Months: {count_month}')
+# Print the net total amount of "Profit/Losses" over the entire period
 print(f'Total: ${ProfitLoss}')
+# Print the average montly "Profit/Losses" over the entire period
 print(f'Average Change: ${round(avg_change,2)}')
+# Print the greatest increase in profits (date and amount) over the entire period
 print(f'Greatest Increase in Profits: {GreatestProfitDate} (${GreatestProfit})')
+# Print the greatest decrease in profits (date and amount) over the entire period
 print(f'Greatest Decrease in Profits: {GreatestLossDate} (${GreatestLoss})')
-
-# Calculate the changes in the "Profit/Losses" over the entire period, then find the average of those changes
-
-# The greatest increase in profits (date and amount) over the entire period
 
 # Learnt to write text files with Tim Smith in study group
 # Reviewed website (https://www.pythontutorial.net/python-basics/python-write-text-file/#:~:text=To%20write%20to%20a%20text%20file%20in%20Python%2C,close%20the%20file%20using%20the%20close%20%28%29%20method)
@@ -87,7 +90,7 @@ output_path = os.path.join("analysis", "PyPollAnalysis.txt")
 # Open the file using "write" mode. Specify the variable to hold the contents
 with open(output_path, 'w') as f:
 
-   #Total # of votes casted
+   # Write the results that were printed earlier to the text file
     f.write(f'Financial Analysis \n')
     f.write(f'---------------------------- \n')
     f.write(f'Total Months: {count_month} \n')
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,23 +1,22 @@
-#Worked on this section of the assignment with the Tutor(Cedric Lutonda)
-#create the file path across operating systems
+# Worked on this section of the assignment with the Tutor(Cedric Lutonda)
+# Create the file path across operating systems
 import os
 
-#the module for reading csv file
+# The module for reading csv file
 import csv
 
-#the path for the csv file
+# The path for the csv file
 csvpath = os.path.join('Resources', 'election_data.csv')
 
 # Use the Improved Reading using CSV module
-
-#Create a dictionary to hold the candidates names
+# Create a dictionary to hold the candidates names
 candidate_votes = dict()
 
-#Create a list of candidates that we find
-#key = candidate; value = vote
+# Create a list of candidates that we find in the csv file
+# key = candidate; value = vote
 candidates_list = []
 
-#initialze the total votes to zero
+# Initialze the total votes to zero
 total_votes = 0
 
 with open(csvpath) as csvfile:
@@ -31,44 +30,49 @@ with open(csvpath) as csvfile:
     # Read each row of data after the header
     for row in csvreader:
 
-        #for each row add one to the number of votes for a total vote count
+        # For each row add one to the number of votes for a total vote count
         total_votes+=1
 
-        #Check to see the candidates name
+        # Check to see the candidates name in column 3
         candidate=row[2]
 
-        #If candidate is not in the list then add the candidate to the candidates_list
+        # If candidate is not in the list then add the candidate to the candidates_list
         if candidate not in candidates_list: 
             candidates_list.append(candidate)
 
-        #Add the candidate to the dictionary if not already in the dictionary
+        # Add the candidate to the dictionary if not already in the dictionary
             candidate_votes[candidate]=0
 
-        #once candidate is already in the list then increase the vote by one
+        # Once candidate is already in the list then increase the vote by one for the candidate each time the name is in colum 3
         candidate_votes[candidate]+=1
         
-#Total # of votes casted
+# Print the results for the election
 print(f'Election Results')
 print(f'-------------------------')
+# Total # of votes casted
 print(f'Total Votes: {total_votes}')
 print(f'-------------------------')
+# Print the name of each candidate, followed by the percentage of votes received, and then print the total votes received
 for candidate in candidates_list:
     candidate_percentage = (candidate_votes[candidate]/total_votes)*100
     print(f'{candidate}: {candidate_percentage:.3f}% ({candidate_votes[candidate]})')
 print(f'-------------------------')
+# Calculate the winner of the election
+# Reviewed webiste (https://datagy.io/python-get-dictionary-key-with-max-value/)
 winner = max(candidate_votes,key=candidate_votes.get)
+# Print the election winner
 print(f'Winner: {winner}')
 print(f'-------------------------')
 
 # Learnt to write text files with Tim Smith in study group
 # Reviewed website (https://www.pythontutorial.net/python-basics/python-write-text-file/#:~:text=To%20write%20to%20a%20text%20file%20in%20Python%2C,close%20the%20file%20using%20the%20close%20%28%29%20method)
-# Specify the file to write to
+# Specify the file to write  the text to
 output_path = os.path.join("analysis", "PyPollAnalysis.txt")
 
 # Open the file using "write" mode. Specify the variable to hold the contents
 with open(output_path, 'w') as f:
 
-   #Total # of votes casted
+   # Save the results printed earlier in the text file
     f.write(f'Election Results \n')
     f.write(f'------------------------- \n')
     f.write(f'Total Votes: {total_votes} \n')
